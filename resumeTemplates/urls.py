23c6dea641e69d503a199ea9b83e416d228b8a9c
premsagar2004 <72159239+premsagar2004@users.noreply.github.com>
2020-10-01 13:58:14 +0530

    Update urls.py

--- resumeTemplates/urls.py
+++ resumeTemplates/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.templates, name='resumeTemplates')
+    path('', views.templates, name='resumeTemplates'),
+    path('', views.res_templates, name='resumeTemplates2')
 ]
